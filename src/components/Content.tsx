5266b53f5dc14ca27aa5d95924dc699e489e1459
Gabriel Tiso <gabrieltisovinhasdebrito2@gmail.com>
2021-03-15 12:59:41 -0300

    :tada: challenge completed. Refactored App into separate components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard'
+
+import { MovieProps } from "../App"
+
+type ContentProps = {
+  movies: MovieProps[];
+  category: string;
+}
+
+export function Content({ movies, category }: ContentProps) {
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {category}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
